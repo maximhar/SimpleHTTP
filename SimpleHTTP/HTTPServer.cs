15868643339e577e8a319371a4851ca8fbf0ee1c
Maxim Harizanov <maxim.harizanov@gmail.com>
2012-11-20 17:21:33 +0200

    Initial commit

---
+++ SimpleHTTP/HTTPServer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Diagnostics;
+
+namespace SimpleHTTP
+{
+    class HTTPServer
+    {
+        IPEndPoint endpoint;
+        Socket serverSocket;
+        NetworkStream stream;
+        StreamWriter writer;
+        StreamReader reader;
+        public HTTPServer(IPAddress bindAddress, int bindPort)
+        {
+            endpoint = new IPEndPoint(bindAddress, bindPort);
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        public void Start()
+        {
+            serverSocket.Bind(endpoint);
+            Socket clientSocket;
+            serverSocket.Listen(10);
+            while (true)
+            {
+                clientSocket = serverSocket.Accept();
+                Stopwatch s = new Stopwatch();
+                if (clientSocket.Connected)
+                {
+                    s.Start();
+                    using (stream = new NetworkStream(clientSocket))
+                    using (writer = new StreamWriter(stream))
+                    using (reader = new StreamReader(stream))
+                    {
+                        if (IsHttpGet())
+                            OnClientHandled(reader, writer);
+                        writer.Flush();
+                        reader.Close();
+                        writer.Close();
+                        stream.Close();
+                    }
+                }
+                clientSocket.Close();
+                s.Stop();
+                Console.WriteLine("Time elapsed: {0}ms.", s.ElapsedMilliseconds);
+            }
+        }
+
+        private bool IsHttpGet()
+        {
+            var firstLine = reader.ReadLine();
+            if (firstLine == null) return false;
+            return firstLine.Contains("GET") && firstLine.Contains("HTTP");
+        }
+
+        private void OnClientHandled(StreamReader reader, StreamWriter writer)
+        {
+            if (ClientConnected != null)
+                ClientConnected(this, new ClientConnectionEventArgs(reader, writer));
+        }
+
+        public event EventHandler<ClientConnectionEventArgs> ClientConnected;
+    }
+}
---
+++ SimpleHTTP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Sockets;
+
+namespace SimpleHTTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HTTPServer server = new HTTPServer(IPAddress.Loopback, 18080);
+            server.ClientConnected += new EventHandler<ClientConnectionEventArgs>(server_ClientConnected);
+            server.Start();
+        }
+
+        static void server_ClientConnected(object sender, ClientConnectionEventArgs e)
+        {
+            Console.WriteLine("Client connected, request: {0}", e.Request.ReadLine());
+            e.Response.WriteLine("HTTP/1.1 200 OK");
+            e.Response.WriteLine();
+            e.Response.WriteLine("This is my response. The game.");
+        }
+    }
+    
+
+}
